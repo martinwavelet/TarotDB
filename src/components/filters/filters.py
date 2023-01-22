06c7be290219e3297a53cd9d9b740dd9ce7c7b5b
Alexandre Wavelet <wavelet.alexandre@gmail.com>
2023-01-22 10:07:31 +0100

    Mobile filter layout

--- src/components/filters/filters.py
+++ src/components/filters/filters.py
@@ -1,15 +1,23 @@
 import dash_bootstrap_components as dbc
-from dash import html
+from dash import html, dcc
 
 from src.components.filters import season_dropdown, taker_dropdown, contract_dropdown
 
 
 def render(app, data):
-    return html.Div(
-        [
-            dbc.Col(season_dropdown.render(app, data)),
-            dbc.Col(taker_dropdown.render(app, data)),
-            dbc.Col(contract_dropdown.render(app, data))
-        ],
-        className="filters"
-    ),
+    return html.Div([
+        dcc.Input(
+            type='checkbox',
+            id="filters-toggle-checkbox",
+            name="filters-toggle-checkbox"
+        ),
+        html.Label(htmlFor="filters-toggle-checkbox", id="filters-toggle"),
+        html.Div(
+            [
+                dbc.Col(season_dropdown.render(app, data)),
+                dbc.Col(taker_dropdown.render(app, data)),
+                dbc.Col(contract_dropdown.render(app, data))
+            ],
+            className="filters"
+        ),
+    ])
